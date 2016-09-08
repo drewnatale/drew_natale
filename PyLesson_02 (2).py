cc39d774669e0a6b43d80b708ebeed2a8a37cf95
drewnatale <nataled0435@my.sduhsd.net>
2016-09-08 10:07:02 -0700

    Add files via upload

---
+++ PyLesson_02 (2).py
@@ -0,0 +1,29 @@
+num1 = 2
+num2 = 8
+print (num1 * num2)
+
+
+
+print =  ( "drew" )
+print =  ( "roselle meadows trail" )
+print = ( "San Diego" )  
+print =  ( "92130" )
+
+
+
+l = 2
+
+w = 5
+
+h = 7
+
+print ( "l * w * h" )
+
+
+
+
+
+
+
+
+
