f13ffb33258735f7f63d650cceae274fb01c3660
drewnatale <nataled0435@my.sduhsd.net>
2016-09-30 09:46:09 -0700

    updated

---
+++ PyLesson_01/firstprog.py
@@ -0,0 +1,3 @@
+print("{o,o}")
+print(" )__)")
+print("_w_w_")
---
+++ PyLesson_02 (2).py
@@ -0,0 +1,34 @@
+
+num1 = 2
+
+num2 = 8
+print (num1 * num2)
+
+name = input ("whats you name?")
+address = input ("whats your address?")
+city = input ("whats your city?")
+zip = input ("whats your zip?")
+
+
+
+length = int(input("what is the length?"))
+width = int(input("what is the width?"))
+height = int(input("whats is the height"))
+
+def problem(l, w, h):
+    return((l*w*h)*2)
+
+
+print("The surface area of your rectangle is: ", problem(length,width,height))
+
+
+
+
+
+
+
+
+
+
+
+
