55367fabb1ec6a0a325706c8acb8bea1c4295956
drewnatale <nataled0435@my.sduhsd.net>
2016-10-04 09:57:49 -0700

    Add files via upload

---
+++ PyLesson_04/average.py
@@ -0,0 +1,41 @@
+def problem(n1,n2,n3):
+    return float((n1 + n2 + n3)/3)
+
+num1= int(input("what is num1?"))
+num2= int(input("what is num2?"))
+num3= int(input("what is num3?"))
+
+print("the average is:" ,problem(num1,num2,num3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
