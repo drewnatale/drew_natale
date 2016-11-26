bca5df82a4d2a567ec1298be8b21afca0de456cd
drewnatale <nataled0435@my.sduhsd.net>
2016-11-26 14:49:12 -0800

    Add files via upload

---
+++ PyLesson_07/Lesson_07digitadder.py
@@ -0,0 +1,14 @@
+number = int(input("what is the number?")
+
+
+sum = 0
+
+num = "number"  
+while number > 0:
+        digits += 1
+    num = int(num / 10)
+
+
+print("the sum of the digits in" , number, "is" , sum)
+
+             
---
+++ PyLesson_07/Lesson_07reversenumber.py
@@ -0,0 +1,13 @@
+number = int(input("what is the number?"))
+
+num = number
+
+rev = 0
+
+while number > 0:
+
+rev * 10
+
+num = int(num / 10)
+
+    
